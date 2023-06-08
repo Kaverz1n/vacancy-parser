046e1003fa06b6be0da9c4862fea91b5425f7624
Dmitry <dima.captan@yandex.ru>
2023-06-08 18:07:01 +0300

    HHAPI moved to hh_api.py and added some bstractmethods

--- src/api.py
+++ src/api.py
@@ -1,5 +1,3 @@
-import requests
-
 from abc import ABC, abstractmethod
 
 
@@ -18,9 +16,9 @@ class API(ABC):
         self.search_params = None
         self.__experience = None
         self.__area = None
-        self.currency = None
+        self.__currency = None
         self.__only_with_salary = False
-        self.salary = None
+        self.__salary = None
 
     @abstractmethod
     def get_vacancies(self):
@@ -46,9 +44,9 @@ class API(ABC):
     @abstractmethod
     def experience(self, value):
         '''
-        Устанавливает значение опыта для
+        Устанавливает id опыта для
         получения вакансии
-        :param value: 'id' опыта
+        :param value: опыт
         '''
         pass
 
@@ -66,9 +64,9 @@ class API(ABC):
     @abstractmethod
     def area(self, value):
         '''
-        Устанавливает значения области поиска
+        Устанавливает id области поиска
         вакансий
-        :param value: 'id' области поиска
+        :param value: область поиска
         '''
         pass
 
@@ -85,129 +83,48 @@ class API(ABC):
     @only_with_salary.setter
     @abstractmethod
     def only_with_salary(self, value):
+        '''
+        Устанавливает значение типа зарплаты вакансии
+        (True - зарплата указана, False - зарплата не указана)
+        :param value: bool-тип
+        '''
         pass
 
-
-class HHAPI(API):
-    '''
-    Класс для работы с API сайта
-    https://hh.ru/
-    '''
-
-    def __init__(self):
+    @property
+    @abstractmethod
+    def currency(self):
         '''
-        Параметры для поиска вакансий
+        Выводит значение выбранной пользователем
+        валюты
+        :return: аббревиатура валюты
         '''
-        self.__api = 'https://api.hh.ru/vacancies'
-        self.search_params = None
-        self.__experience = None
-        self.__area = None
-        self.currency = None
-        self.__only_with_salary = False
-        self.salary = None
+        pass
 
-    def get_vacancies(self):
+    @currency.setter
+    @abstractmethod
+    def currency(self, value):
         '''
-        Возвращает список вакансий, созданных на
-        основе класса Vacancy по переданным параметрам
-        пользователя
-        :return: список объектов класса Vacancy
+        Устанавливает валюту, относительно выбраного числа
+        :param value: выбранное число пользователем
         '''
-        vacancies = requests.get(
-            url=self.__api,
-            params={
-                "text": self.search_params,
-                "experience": self.__experience,
-                "area": self.__area,
-                "currency": self.currency,
-                "only_with_salary": self.only_with_salary,
-                "salary": self.salary
-            }
-        ).json()
-
-        return vacancies
+        pass
 
     @property
-    def experience(self):
+    @abstractmethod
+    def salary(self):
         '''
-        Возвращает значение необходимого
-        опыта для получения вакансии
-        :return: опыт
+        Возвращает значение желаемой зарплаты,
+        переданной пользователем
+        :return: значение зарплаты
         '''
-        experiences = requests.get(url='https://api.hh.ru/dictionaries').json()['experience']
+        pass
 
-        # перебирает словарь с опытом для получения вакансии
-        for experience in experiences:
-            if experience['id'] == self.__experience:
-                return experience['name']
-
-    @experience.setter
-    def experience(self, value):
+    @salary.setter
+    @abstractmethod
+    def salary(self, value):
         '''
-        Устанавливает значение опыта для
-        получения вакансии
-        :param value: номер опыта
+        Устанавливает значение желаемой зарплаты,
+        если оно больше 0 и является числом
+        :param value: значение желаемой зарплаты
         '''
-        experiences = requests.get(url='https://api.hh.ru/dictionaries').json()['experience']
-
-        # перебирает словарь с опытом для получения вакансии
-        for experience in experiences:
-            if experiences.index(experience) + 1 == value:
-                self.__experience = experience['id']
-
-    @property
-    def area(self):
-        '''
-        Возвращает значение области поиска
-        вакансий
-        :return: область поиска
-        '''
-        countries = requests.get(url='https://api.hh.ru/areas').json()
-
-        # цикл перебирает регионы страны Россия
-        for country in countries[0]['areas']:
-            if country['id'] == self.__area:
-                return country['name']
-
-            # цикл перебирает города в областях
-            for city in country['areas']:
-                if city['id'] == self.__area:
-                    return city['name']
-
-    @area.setter
-    def area(self, value):
-        '''
-        Устанавливает id области
-        поиска вакансий
-        '''
-        countries = requests.get(url='https://api.hh.ru/areas').json()
-
-        # цикл перебирает регионы страны Россия
-        for country in countries[0]['areas']:
-            if country['name'] == value.title():
-                self.__area = country['id']
-
-            # цикл перебирает города в регионах
-            for city in country['areas']:
-                if city['name'] == value.title():
-                    self.__area = city['id']
-
-    @property
-    def only_with_salary(self):
-        '''
-        Возвращает значение типа зарплаты вакансии
-        (True - зарплата указана, False - зарплата не указана)
-        :return: bool-тип зарплаты
-        '''
-        return self.__only_with_salary
-
-    @only_with_salary.setter
-    def only_with_salary(self, value):
-        '''
-        Устанавливает значения типа зарплаты вакансии,
-        взависимости от ответа пользователя(Если "да", то
-        True, иначе деффолтное значение False)
-        :param value: ответ пользователя
-        '''
-        if value.lower() == 'да':
-            self.__only_with_salary = True
+        pass
