eccce67641362e6fb3f1970289442a48d3dc6ec1
Dmitry <dima.captan@yandex.ru>
2023-06-11 13:52:14 +0300

    added pagination

--- src/hh_api.py
+++ src/hh_api.py
@@ -13,13 +13,15 @@ class HHAPI(API):
         '''
         Параметры для поиска вакансий
         '''
-        self.__api = 'https://api.hh.ru/vacancies'
+        self.__api = 'https://api.hh.ru'
         self.search_params = None
         self.__experience = None
         self.__area = None
         self.__currency = None
         self.__only_with_salary = False
         self.__salary = None
+        self.__page = 0
+        self.__per_page = 100
 
     def __str__(self):
         return 'API сайта https://hh.ru/'
@@ -34,17 +36,30 @@ class HHAPI(API):
         пользователя
         :return: список объектов класса Vacancy
         '''
-        vacancies = requests.get(
-            url=self.__api,
-            params={
-                "text": self.search_params,
-                "experience": self.__experience,
-                "area": self.__area,
-                "only_with_salary": self.__only_with_salary,
-                "currency": self.__currency,
-                "salary": self.__salary
-            }
-        ).json()
+        vacancies = []
+
+        while True:
+            response = requests.get(
+                url=f'{self.__api}/vacancies',
+                params={
+                    "text": self.search_params,
+                    "experience": self.__experience,
+                    "area": self.__area,
+                    "only_with_salary": self.__only_with_salary,
+                    "currency": self.__currency,
+                    "salary": self.__salary,
+                    "page": self.__page,
+                    "per_page": self.__per_page
+                }
+            ).json()
+
+            if self.__page == response['pages']:
+                break
+
+            self.__page += 1
+
+            for vacancy in response['items']:
+                vacancies.append(vacancy)
 
         return vacancies
 
@@ -55,7 +70,7 @@ class HHAPI(API):
         опыта для получения вакансии
         :return: опыт
         '''
-        experiences = requests.get(url='https://api.hh.ru/dictionaries').json()['experience']
+        experiences = requests.get(url=f'{self.__api}/dictionaries').json()['experience']
 
         # перебирает словарь с опытом для получения вакансии
         for experience in experiences:
@@ -69,7 +84,7 @@ class HHAPI(API):
         вакансии
         :param value: номер опыта из списка
         '''
-        experiences = requests.get(url='https://api.hh.ru/dictionaries').json()['experience']
+        experiences = requests.get(url=f'{self.__api}/dictionaries').json()['experience']
 
         try:
             # перебирает словарь с опытом для получения вакансии
@@ -86,7 +101,7 @@ class HHAPI(API):
         вакансий
         :return: область поиска
         '''
-        countries = requests.get(url='https://api.hh.ru/areas').json()
+        countries = requests.get(url=f'{self.__api}/areas').json()
 
         # цикл перебирает регионы страны Россия
         for country in countries[0]['areas']:
@@ -105,7 +120,7 @@ class HHAPI(API):
         поиска вакансий
         :param value: область поиска
         '''
-        countries = requests.get(url='https://api.hh.ru/areas').json()
+        countries = requests.get(url=f'{self.__api}/areas').json()
 
         # цикл перебирает регионы страны Россия
         for country in countries[0]['areas']:
@@ -154,7 +169,7 @@ class HHAPI(API):
         Устанавливает валюту, относительно выбраного числа
         :param value: выбранное число пользователем
         '''
-        currencies = requests.get(url='https://api.hh.ru/dictionaries').json()['currency']
+        currencies = requests.get(url=f'{self.__api}/dictionaries').json()['currency']
 
         try:
             # перебирает словарь с валютами
