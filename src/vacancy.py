99cc017b4db3464a7abba6649c956d48a4cfa65d
Dmitry <dima.captan@yandex.ru>
2023-06-13 16:22:09 +0300

    added new methods

--- src/vacancy.py
+++ src/vacancy.py
@@ -2,6 +2,7 @@ class Vacancy:
     '''
     Класс вакансий
     '''
+    vacancies = []
 
     def __init__(
             self, name, requirement, responsibility,
@@ -35,6 +36,8 @@ class Vacancy:
         self.__employment = employment
         self.__address = address
 
+        Vacancy.vacancies.append(self)
+
     def __repr__(self):
         return f'{self.__class__.__name__}({self.name}, {self.__requirement},' \
                f'{self.__responsibility}, {self.__area}, {self.__salary_from},' \
@@ -44,13 +47,98 @@ class Vacancy:
     def __str__(self):
         return f'Вакансия {self.name}'
 
+    def __eq__(self, other):
+        '''
+        Метод сравнивает зарплаты двух вакансий
+        :param other: вакансия с которой сравнивают
+        :return: результат
+        '''
+        try:
+            if self.salary.split()[0] == other.salary.split()[0]:
+                return 'Зарплаты одинаковы'
+            else:
+                return 'Зарплаты не равны'
+        except ValueError:
+            return 'Указаны неверные объекты или у одного из объектов отсутсвует зарплата'
+
+    def __lt__(self, other):
+        '''
+        Метод проверяет является ли зарплата первой вакансии
+        меньше, чем зарплата второй вакансии
+        :param other: вакансия с которой сравнивают
+        :return: результат
+        '''
+        try:
+            first_salary = int(self.salary.split()[0])
+            second_salary = int(other.salary.split()[0])
+
+            if first_salary < second_salary:
+                difference = second_salary - first_salary
+                return f'{other.name} больше на {difference} {other.salary.split()[1]}'
+            else:
+                difference = first_salary - second_salary
+                return f'{self.name} больше на {difference} {self.salary.split()[1]}'
+        except ValueError:
+            return 'Указаны неверные объекты или у одного из объектовотсутсвует зарплата'
+
+    def __le__(self, other):
+        '''
+        Метод проверяет является ли зарплата первой вакансии
+        меньше или равна, чем зарплата второй вакансии
+        :param other: вакансия с которой сравнивают
+        :return: результат
+        '''
+        try:
+            first_salary = int(self.salary.split()[0])
+            second_salary = int(other.salary.split()[0])
+
+            if first_salary == second_salary:
+                return 'Зарплаты равны'
+            else:
+                return self.__lt__(other)
+        except ValueError:
+            return 'Указаны неверные объекты или у одного из объектовотсутсвует зарплата'
+
+    def __gt__(self, other):
+        '''
+        Метод проверяет является ли зарплата первой вакансии
+        больше, чем зарплата второй вакансии
+        :param other: вакансия с которой сравнивают
+        :return: результат
+        '''
+        try:
+            first_salary = int(self.salary.split()[0])
+            second_salary = int(other.salary.split()[0])
+
+            if first_salary > second_salary:
+                difference = first_salary - second_salary
+                return f'{other.name} больше на {difference} {other.salary.split()[1]}'
+            else:
+                difference = second_salary - first_salary
+                return f'{self.name} больше на {difference} {self.salary.split()[1]}'
+        except ValueError:
+            return 'Указаны неверные объекты или у одного из объектовотсутсвует зарплата'
+
+    def __ge__(self, other):
+        try:
+            first_salary = int(self.salary.split()[0])
+            second_salary = int(other.salary.split()[0])
+
+            if first_salary == second_salary:
+                return 'Зарплаты равны'
+            else:
+                return self.__gt__(other)
+
+        except ValueError:
+            return 'Указаны неверные объекты или у одного из объектовотсутсвует зарплата'
+
     @property
     def description(self):
         '''
         Возвращает информацию о вакансии
         :return: информация вакансии
         '''
-        return f'ТРЕБОВАНИЯ:\n{self.__requirement}\nИНФОРМАЦИЯ:\n{self.__responsibility}'
+        return f'Информация: {self.__requirement}\n{self.__responsibility}'
 
     @property
     def area(self):
@@ -112,3 +200,41 @@ class Vacancy:
                f'{self.area} по адресу: {self.adress}\nТребует опыт: ' \
                f'{self.experience} и занятость: {self.employment}\n' \
                f'Работадатель: {self.employer}'
+
+    @classmethod
+    def get_min_salary_vacancy(cls):
+        '''
+        Метод выводит минимальную зарплату. Использовать, если
+        у всех вакансий одинаковая валюта и указана зарплата
+        :return:
+        '''
+        min_salary = int(cls.vacancies[0].salary.split()[0]) if cls.vacancies else None
+        min_vacancy = cls.vacancies[0] if cls.vacancies else None
+        for vacancy in cls.vacancies:
+            try:
+                if int(vacancy.salary.split()[0]) < min_salary:
+                    min_salary = int(vacancy.salary.split()[0])
+                    min_vacancy = vacancy
+            except ValueError:
+                continue
+
+        return min_vacancy
+
+    @classmethod
+    def get_max_salary_vacancy(cls):
+        '''
+        Метод выводит максимальную зарплату. Использовать, если
+        у всех вакансий одинаковая валюта и указана зарплата
+        :return:
+        '''
+        max_salary = int(cls.vacancies[0].salary.split()[0]) if cls.vacancies else None
+        min_vacancy = cls.vacancies[0] if cls.vacancies else None
+        for vacancy in cls.vacancies:
+            try:
+                if int(vacancy.salary.split()[0]) > max_salary:
+                    max_salary = int(vacancy.salary.split()[0])
+                    min_vacancy = vacancy
+            except ValueError:
+                continue
+
+        return min_vacancy
